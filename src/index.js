34830b0990273108219d990c2d00a83e18e2a3dd
Marr <marat.salah1313@gmail.com>
2023-03-02 18:11:13 +0100

    update evenGame.js and add index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const engine = (rules, logicOfGame) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
+  let i = 0;
+  while (i <= 2) {
+    const [question, trueAnswer] = logicOfGame();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === trueAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default engine;
