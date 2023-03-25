a0f558a2bca9e347edb97117f30c2ccd7d0dd30e
Marr <marat.salah1313@gmail.com>
2023-03-25 19:12:15 +0100

    add refactored primeGame.js

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -3,19 +3,17 @@ import _ from 'lodash';
 export const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  let result;
-  for (let i = number - 1; i >= 1; i -= 1) {
-    if ((number) % i !== 0) {
-      result = true;
-    } else if (i === 1) {
-      result = true;
-      return result;
-    } else if ((number) % i === 0) {
-      result = false;
-      return result;
+  if (number <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
   }
-  return result;
+
+  return true;
 };
 
 export const primeGame = () => {
